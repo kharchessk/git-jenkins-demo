5209f65f7876767727cf7e9cb857e84701dfaa13
kharchessk <kharchessk@gmail.com>
2020-08-10 11:00:52 +0530

    git jenkins auto version

---
+++ src/main/java/git/jenkins/auto/versioning/AutoVersioningApplication.java
@@ -0,0 +1,13 @@
+package git.jenkins.auto.versioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoVersioningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/git/jenkins/auto/versioning/AutoVersioningApplicationTests.java
@@ -0,0 +1,13 @@
+package git.jenkins.auto.versioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoVersioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
